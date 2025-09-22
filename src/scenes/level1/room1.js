0a3b180f134e0b367aa0bd1b3d6d0a3e9de3e511
MualusiLilimu <lilimumualusi37@gmail.com>
2025-09-23 01:55:17 +0200

    added incomplete room1

--- src/scenes/level1/room1.js
+++ src/scenes/level1/room1.js
@@ -6,21 +6,53 @@ import {loadTexture} from '../../../utils/loader.js';
 
 
 
+
+
 // everything you want to design for this ROOM you should do it inside createRoom function
 // you are also free to create functions outside the createRoom function and call them inside afterwards
+
+
+// function to create walls
+function createWall(width, height, depth, x, y, z, paint,path) {
+  const wallgeometry = new THREE.BoxGeometry(width, height, depth);
+  const wallmaterial = new THREE.MeshPhongMaterial({ color: paint,map: path});
+  const wall = new THREE.Mesh(wallgeometry, wallmaterial);
+
+  wall.position.set(x, y, z);
+
+  return wall;
+}
+
+// function to create a floor
+function createFloor(width, height, depth, x, y, z, paint,path){
+  const floorgeometry = new THREE.BoxGeometry(width, height, depth);
+  const floormaterial = new THREE.MeshPhongMaterial({ color: paint,map: path});
+  const floor = new THREE.Mesh(floorgeometry, floormaterial);
+
+  floor.position.set(x, y, z);
+
+  return floor
+}
 
 export function createRoom1() {
     const room = new THREE.Group();
     //TODO design a roome here
 
-    const floor = loadTexture('/../../public/textures/tile.jpg');
-    const geometry = new THREE.BoxGeometry(40,1,40);
-    const material = new THREE.MeshPhongMaterial({color: "red"});
+    const floortexture = loadTexture('../../../../public/textures/tile.jpg');
+    const walltexture = loadTexture('');
+    // floor
+    const floor = createFloor(30,1,30,0,0,0,"gray",floortexture); 
+    room.add(floor);
 
-    const cube = new THREE.Mesh(geometry, material);
+    // walls
+    const wall1 = createWall(30,15,1,0,8,-14.5,"red",walltexture);
+    room.add(wall1);
 
+    const wall2 = createWall(1,15,30,14.5,8,0,"red",walltexture);
+    room.add(wall2);
 
-    room.add(cube);
+    const wall3 = createWall(1,15,30,-14.5,8,0,"red",walltexture);
+    room.add(wall3);
 
   return room;
 }
